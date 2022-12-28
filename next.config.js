4cb9c7f782d7ce282366f930c81904beab93dcdd
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2022-12-28 16:42:51 +1100

    feat: Support reading markdown files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: config => {
+    config.externals['node:fs'] = false;
+    config.externals['node:path'] = false;
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
