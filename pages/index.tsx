a4e702e9da560a39e53f1057d665b51cee41fddb
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2023-01-02 16:36:07 +1100

    feat: Add blobs back in

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,44 @@ import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 // import { Inter } from '@next/font/google';
 // const inter = Inter({ subsets: ['latin'] });
+const blobMask = (animationDisabled: boolean) => `
+<?xml version="1.0" encoding="UTF-8"?>
+<svg width="0" height="0" viewBox="0 0 350 350" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <clipPath id="Blobby" clipPathUnits="objectBoundingBox" transform="scale(0.002857143, 0.002857143)">
+      <path class="clip" d="M340.359196,232.907053 C369.892183,148.680525 331.647945,55.2974307 224.595191,15.3076571 C127.999397,-20.7822144 15.0584823,2.4783672 1.02368828,133.257611 C-8.11390749,218.443339 44.9256302,318.452481 119.96223,341.542678 C192.34282,368.494915 306.073047,330.694892 340.359196,232.907053 Z">
+        <animate
+          id="GentleMorph"
+          dur="${animationDisabled ? 0 : 35}"
+          begin="0"
+          repeatCount="indefinite"
+          attributeName="d"
+          values="M340.359196,232.907053 C369.892183,148.680525 331.647945,55.2974307 224.595191,15.3076571 C127.999397,-20.7822144 15.0584823,2.4783672 1.02368828,133.257611 C-8.11390749,218.443339 44.9256302,318.452481 119.96223,341.542678 C192.34282,368.494915 306.073047,330.694892 340.359196,232.907053 Z;
+
+                  m334.719465 233.064353c28.871403-82.339727 22.417835-142.7382456-65.204606-209.5226813-79.809275-60.8293649-253.8096353 20.7578982-267.53002864 148.6074893-8.93289979 83.277438 43.91922794 144.544272 117.27489464 167.117214 70.759155 26.348465 181.941653-10.60478 215.45974-106.202022z;
+
+                  M340.359196,232.907053 C369.892183,148.680525 331.647945,55.2974307 224.595191,15.3076571 C127.999397,-20.7822144 15.0584823,2.4783672 1.02368828,133.257611 C-8.11390749,218.443339 44.9256302,318.452481 119.96223,341.542678 C192.34282,368.494915 306.073047,330.694892 340.359196,232.907053 Z;" />
+      </path>
+    </clipPath>
+  </defs>
+</svg>
+`;
+
+const BlobBackground = () => {
+  const PREFERS_REDUCED_MOTION =
+    typeof window !== 'undefined' &&
+    typeof window.matchMedia === 'function' &&
+    window.matchMedia('(prefers-reduced-motion)').matches;
+
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: blobMask(PREFERS_REDUCED_MOTION) }}
+    />
+  );
+};
+const Blob = ({ className }: { className: string }) => {
+  return <div className={className} style={{ clipPath: `url(#Blobby)` }} />;
+};
 
 const Home = () => {
   return (
@@ -18,12 +56,12 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        {/*
+        <div className={styles.blobContainer}>
           <BlobBackground />
           <Blob className={styles.blob1} />
           <Blob className={styles.blob2} />
           <Blob className={styles.blob3} />
-          */}
+        </div>
         <div className={styles.wrapper}>
           <div className={styles.main}>
             <h1>Hello! My name is Randeep</h1>
